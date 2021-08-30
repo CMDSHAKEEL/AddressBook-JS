6bee16120059f22fbf3922af88274437dbf7972e
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-30 18:06:02 +0530

    Welcome-To-AddressBook-JS

---
+++ AddressBook.js
@@ -0,0 +1,20 @@
+//Creating Address book contacts
+class AddressBook {
+    constructor(...params) {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
+    }
+
+    toString() {
+        return "First Name : " + this.firstName + "\nLast Name : " + this.lastName + "\nAddress : " + this.address + "\nCity : " + this.city + "\nState : " + this.state + "\nZip : " + this.zip + "\nPhone Number : " + this.phoneNumber + "\nEmail : " + this.email;
+    }
+}
+
+let addressBook = new AddressBook("CMD", "SHAKEEL", "ADONI", "Kurnool", "AP", "251031", "9581477386", "cmdshakeel21@gmail.com");
+console.log(addressBook.toString());
